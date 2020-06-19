b6bdd9acbdb721409c3f96c1764939394074945a
kwg03242 <kwg03242@DESKTOP-5IPA4M8.localdomain>
2020-06-20 07:43:37 +0900

    test bar chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import * as d3 from "d3";
 
-const margin = ({top: 20, right: 30, bottom: 30, left: 40})
+const margin = ({top: 40, right: 50, bottom: 50, left: 60});
 const height = 700;
 const width = 1000;
 
@@ -44,8 +44,7 @@ export function ProfitChart( { profits = [] } ) {
 
                 const svg = d3.select(d3Container.current)
                     .attr("viewBox", [0, 0, width, height])
-                    .attr("width", width)
-                    .attr("height", height);
+
 
                 svg.append("path")
                     .datum(data)
@@ -164,6 +163,65 @@ export function BTCValueChart( { prices = [] } ) {
         }
     }, [prices])
     
+    return (
+        <svg ref={d3Container} />
+    );
+}
+
+export function BarChart ( { prices = [] } ) {
+    const d3Container = useRef(null);
+    useEffect(() => {
+        if (prices.length > 0 && d3Container.current) {           
+            
+            const data = Object.assign(prices.map(({date, price}) => ({date, value: price})), {format: "$", y: "USD"});
+            console.log(data);
+            const x = d3.scaleBand()
+                .domain(d3.range(data.length))
+                .range([margin.left, width - margin.right])
+                .padding(0.1)
+            
+            const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.value)]).nice()
+                .range([height - margin.bottom, margin.top])
+
+            const xAxis = g => g
+                .attr("transform", `translate(0,${height - margin.bottom})`)
+                .call(d3.axisBottom(x).tickFormat(i => data[i].date).tickSizeOuter(0))
+
+            const yAxis = g => g
+                .attr("transform", `translate(${margin.left},0)`)
+                .call(d3.axisLeft(y).ticks(null, data.format))
+                .call(g => g.select(".domain").remove())
+                .call(g => g.append("text")
+                    .attr("x", -margin.left)
+                    .attr("y", 10)
+                    .attr("fill", "currentColor")
+                    .attr("text-anchor", "start")
+                    .text(data.y))
+
+            const color = "steelblue"
+
+            const svg = d3.select(d3Container.current)
+                .attr("viewBox", [0, 0, width, height]);
+
+            svg.append("g")
+                .attr("fill", color)
+                .selectAll("rect")
+                .data(data)
+                .join("rect")
+                .attr("x", (d, i) => x(i))
+                .attr("y", d => y(d.value))
+                .attr("height", d => y(0) - y(d.value))
+                .attr("width", x.bandwidth());
+
+            svg.append("g")
+                .call(xAxis);
+
+            svg.append("g")
+                .call(yAxis);
+        }
+    }, [prices])
+
     return (
         <svg ref={d3Container} />
     );
